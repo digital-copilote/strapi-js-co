ca45346ef86defbf33ea0aa05692791b5861fc85
JIdayyy <julien_abbadie@hotmail.fr>
2021-08-11 10:06:42 +0200

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9bf9acfe1dd848c2d3ddce2be0485708'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5a1b08f0-66d0-4b5e-bcee-222d7ca2a46f'
+};

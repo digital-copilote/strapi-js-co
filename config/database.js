3d6ee1f988949713cd26d33ba2511a2fcb2ed61c
JIdayyy <julien_abbadie@hotmail.fr>
2021-08-19 15:58:28 +0200

    env variable

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "postgres"),
-        schema: env("DATABASE_SCHEMA", "public"), // Not Required
+        host: env("DATABASE_HOST", process.env.DATABASE_HOST),
+        port: env.int("DATABASE_PORT", process.env.DATABASE_PORT),
+        database: env("DATABASE_NAME", process.env.DATABASE_NAME),
+        username: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
+        password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
+        schema: env("DATABASE_SCHEMA", process.env.DATABASE_SCHEMA), // Not Required
         ssl: env("DATABASE_SSL, false"),
       },
       options: {
